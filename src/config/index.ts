2d40c6a4ed8310cdff03e9c81f6b733c2eadfc80
rafi10hasan <rafihasan0075@gmail.com>
2025-06-02 17:59:09 +0600

    updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,3 @@
-
 import dotenv from 'dotenv';
 import path from 'path';
 import { z } from 'zod';
@@ -14,12 +13,14 @@ const envSchema = z.object({
   SERVER_URL: z.string().min(1, 'Server URL is required'),
   FRONTEND_URL: z.string().min(1, 'frontend url is required'),
   MONGODB_URL: z.string().min(1, 'MongoDB connection URL is required'),
-  
+
   JWT_ACCESS_TOKEN_SECRET: z.string().min(1, 'Access token secret key is required'),
   JWT_ACCESS_TOKEN_EXPIRESIN: z.string().default('14d'),
   JWT_REFRESH_TOKEN_SECRET: z.string().min(1, 'Refresh token secret key is required'),
   JWT_REFRESH_TOKEN_EXPIRESIN: z.string().default('30d'),
-  
+
+  STRIPE_SECRET_KEY: z.string().min(1, 'stripe secret key is required'),
+  STRIPE_WEBHOOK_SECRET: z.string().min(1, 'stripe secret key is required'),
   GMAIL_APP_USER: z.string().email('Invalid email format'),
   GMAIL_APP_PASSWORD: z.string().min(1, 'Gmail app password is required'),
 });
@@ -31,15 +32,15 @@ export default {
   server_port: envVars.PORT,
   server_name: envVars.SERVER_NAME,
   mongodb_url: envVars.MONGODB_URL,
-  frontend_url :envVars.FRONTEND_URL,
+  frontend_url: envVars.FRONTEND_URL,
 
   jwt_access_token_secret: envVars.JWT_ACCESS_TOKEN_SECRET,
   jwt_access_token_expiresin: envVars.JWT_ACCESS_TOKEN_EXPIRESIN,
   jwt_refresh_token_secret: envVars.JWT_REFRESH_TOKEN_SECRET,
   jwt_refresh_token_expiresin: envVars.JWT_REFRESH_TOKEN_EXPIRESIN,
-   
+  stripe_webhook_secret: envVars.STRIPE_WEBHOOK_SECRET,
+  stripe_secret_key: envVars.STRIPE_SECRET_KEY,
   gmail_app_user: envVars.GMAIL_APP_USER,
   gmail_app_password: envVars.GMAIL_APP_PASSWORD,
   server_url: envVars.SERVER_URL,
-
 };
